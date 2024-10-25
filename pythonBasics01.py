4b763e757ebf07b8606c8e68050e37ea5627a358
Shah Mohammad Rizvi <22203174@iubat.edu>
2024-10-25 11:11:47 +0600

    pythonBasics01.py

--- pythonBasics01.py
+++ pythonBasics01.py
@@ -3,7 +3,7 @@
 print('Welcome to smartBusiness')
 name=input("What is your name? ") #received input (input always returns strings
 print("Hi " + name) #stringInterpolation
-age=int(input("What is your age? "))
+age=int(input("What is your age? ")) #typecasting string to int
 number=int(input("Enter your number: "))
 print(name + " Your Details are-"
       +"Age: " + str(age) +" Number:" + str(number)) #typecasting or type conversion
